51fb41fc767ba9ca017162bc3c4872a455be21fb
Sergey Orlov <grcss@rambler.ru>
2019-12-03 23:46:19 +0300

    gachi_01

---
+++ another-new-year/script.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  var music = new Audio('./let_it_snow.mp3');
+  var playToggle = false;
+
+  $(document).on('click', '.play', function() {
+    if (!playToggle) {
+      music.play();
+      playToggle = true;
+      $(this).text('Приостановить музыку');
+    } else {
+      music.pause();
+      playToggle = false;
+      $(this).text('Включить музыку');
+    }
+  });
+
+  var $container = $('#snowfall');
+
+  var timer = setTimeout(function tick() {
+    createFlake();
+    timer = setTimeout(tick, randomInteger(500, 2000))
+  }, randomInteger(500, 2000));
+
+  function createFlake() {
+    var randomId = Math.floor(Math.random() * 1e10),
+        percent = Math.floor(Math.random() * 100);
+
+    var direction = randomInteger(0, 1) ? 'mr-smith' : 'mr-smith _r';
+
+    $container.append('<div id="'+randomId+'" class="'+direction+'" style="-webkit-transform: scale(1.'+randomInteger(0, 99)+');-ms-transform: scale(1.'+randomInteger(0, 99)+');transform: scale(1.'+randomInteger(0, 99)+');left:'+percent+'%;-webkit-animation-duration: '+randomInteger(7, 15)+'s;animation-duration: '+randomInteger(7, 15)+'s;"><span style="-webkit-animation-duration: '+randomInteger(5, 20)+'s;animation-duration: '+randomInteger(5, 20)+'s;"></span></div>');
+
+    setTimeout(function() {
+      $('#'+randomId).remove();
+    }, 15000)
+  }
+
+  function randomInteger(min, max) {
+    var rand = min + Math.random() * (max + 1 - min);
+    rand = Math.floor(rand);
+    return rand;
+  }
+});
--- cats/script.js
+++ cats/script.js
@@ -8,27 +8,35 @@ var startDate = new Date('Fri Nov 01 2019 21:39:35 GMT+0300 (Москва, ст�
     doubler = false,
     doublerCounter = 0;
 
-for (var i = 0; i<=366; i++) {
-    var day = new Date(startDate.setDate(startDate.getDate() + 1));
-
-    (triper && triperCounter < 3) && (doubler && doublerCounter < 2) ?  dateArray.push({date: day.getFullYear()+'-'
-            +(day.getMonth() >= 10 ? (day.getMonth()+1) : '0'+(day.getMonth()+1))+'-'
-            +(day.getDate() >= 10 ? day.getDate() : '0'+day.getDate())}) : null;
-    triperCounter++;
-    if (triperCounter === 3) {
-        triper = !triper;
-        triperCounter = 0;
-    }
-    doublerCounter++;
-    if (doublerCounter === 2) {
-        doubler = !doubler;
-        doublerCounter = 0;
-    }
-}
-
 var myCalendar = new VanillaCalendar({
     selector: "#myCalendar",
-    availableDates: dateArray,
     datesFilter: true
 });
-console.log(dateArray);
+
+document.addEventListener('DOMContentLoaded', function () {
+    CheckDate();
+});
+
+var CheckDate = function () {
+    for (var i = 0; i<=366; i++) {
+        var day = new Date(startDate.setDate(startDate.getDate() + 1));
+
+        (triper && triperCounter < 3) && (doubler && doublerCounter < 2) ?  dateArray.push({date: day.getFullYear()+'-'
+                +(day.getMonth() >= 10 ? (day.getMonth()+1) : '0'+(day.getMonth()+1))+'-'
+                +(day.getDate() >= 10 ? day.getDate() : '0'+day.getDate())}) : null;
+        triperCounter++;
+        if (triperCounter === 3) {
+            triper = !triper;
+            triperCounter = 0;
+        }
+        doublerCounter++;
+        if (doublerCounter === 2) {
+            doubler = !doubler;
+            doublerCounter = 0;
+        }
+    }
+    myCalendar.set({
+        availableDates: dateArray,
+    });
+};
+
